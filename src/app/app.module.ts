eff86003023631332c047d8c253af8f0597e74c9
Tyler Torres <tyler.john.torres@gmail.com>
2018-01-03 15:44:37 -0600

    View is working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { FormsModule } from "@angular/forms";
 import { HttpClient, HttpClientModule } from "@angular/common/http";
+import { TodoService } from "./services/todo.service";
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 
@@ -9,8 +10,8 @@ import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, FormsModule, NgbModule.forRoot()],
-  providers: [],
+  imports: [HttpClientModule, BrowserModule, FormsModule, NgbModule.forRoot()],
+  providers: [TodoService],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
